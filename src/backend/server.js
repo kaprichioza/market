6d2c8c7f59dffda83883108386bf1ff45a21b2e9
kaprichioza <nnicead@gmail.com>
2021-04-07 19:43:47 +0300

    add product API

--- src/backend/server.js
+++ src/backend/server.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser')
+const path = require('path');
 const app = express();
+
+const low = require('lowdb')
+const FileSync = require('lowdb/adapters/FileSync')
+const adapter = new FileSync(path.join(__dirname, 'db', 'db.json'))
+const db = low(adapter)
 
 app.get('/', function (req, res) {
   return res.send('ok'); 
 });
+app.get('/api/v1/products', function (req, res) {
+    const products = db.get('products'); 
+    return res.json(products);
+  });
 
 app.listen(process.env.PORT || 8080);
