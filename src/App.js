e5bb7ad6c05f329c70c17a7864c521298da39577
kaprichioza <nnicead@gmail.com>
2021-04-07 20:52:30 +0300

    add auth

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
+import { useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import "./App.css";
 import { BasketPage } from './components/basketPage/basketPage';
 import { CatalogPage } from './components/catalogPage/catalogPage';
 import { Header } from "./components/header/header";
+import { guid } from './utils/guid';
+export const cookieName = 'sessionToken';
 
-function App() {  
+function App() {
+  useEffect(() => {
+    const cookieRegExp = new RegExp(cookieName);
+    if (cookieRegExp.test(document.cookie)) return;
+    const tokenValue = guid()
+    document.cookie=`${cookieName}=${tokenValue}`;
+  }, []);
   return (
-    <Router>      
-        <Header />
-        <Switch>
-          <Route exact path="/">
-            <CatalogPage />
-          </Route>
-          <Route path="/basket">
-            <BasketPage />
-          </Route>
-        </Switch>    
+    <Router>
+      <Header />
+      <Switch>
+        <Route exact path="/">
+          <CatalogPage />
+        </Route>
+        <Route path="/basket">
+          <BasketPage />
+        </Route>
+      </Switch>
     </Router>
   );
 }
